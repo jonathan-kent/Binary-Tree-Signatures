63f66a5b4a11b74238c0d0ead91d00c83ad385f7
Jonathan <jonathanskent@gmail.com>
2020-06-22 20:16:17 -0700

    Able to add nodes

--- main.js
+++ main.js
@@ -118,16 +118,17 @@ function renderTree(node, topOffset, leftOffset, parentOffset, depth){
     line.setAttribute("x1", leftOffset);
     line.setAttribute("y1", topOffset);
     line.setAttribute("x2", parentOffset);
-    line.setAttribute("y2", topOffset-30);
+    line.setAttribute("y2", topOffset-50);
     document.getElementById("svg").appendChild(line);
   }
   var renderNode = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   renderNode.setAttribute("cx", leftOffset);
   renderNode.setAttribute("cy", topOffset);
+  renderNode.addEventListener("click", function(){clickHandler(renderNode, node)});
   document.getElementById("svg").appendChild(renderNode);
   depth++;
-  renderTree(node.left, topOffset+30, leftOffset - (width/Math.pow(2, depth)), leftOffset, depth);
-  renderTree(node.right, topOffset+30, leftOffset + (width/Math.pow(2, depth)), leftOffset, depth);
+  renderTree(node.left, topOffset+50, leftOffset - (width/Math.pow(2, depth)), leftOffset, depth);
+  renderTree(node.right, topOffset+50, leftOffset + (width/Math.pow(2, depth)), leftOffset, depth);
   return;
 }
 
@@ -139,7 +140,9 @@ function fromForm(){
       alert("Invalid Tree Signature");
       return;
     }
-    signature = (parseInt(signature) >>> 0).toString(2);
+    signature = decToBin(signature);
+    console.log(signature);
+    //signature = (parseInt(signature) >>> 0).toString(2);
     if (signature.length % 2 == 1){
       signature = "0" + signature;
     }
@@ -148,5 +151,91 @@ function fromForm(){
   if (newTree[0] != null){
     document.getElementById("svg").innerHTML = "";
     renderTree(newTree[0].root, 30, null, null, 1);
+    globalTree = newTree[0];
+    var signature = preOrder(globalTree.root, "");
+    var display = document.getElementById("signatureDisplay");
+    display.innerHTML = "Current Binary Signature: "+signature+"\
+                        <br>Current Decimal Signature: "+ parseInt(signature, 2);
   }
 }
+
+
+function decToBin(decimal){
+  decimal = parseInt(decimal);
+  var binary = "";
+  while(decimal > 0){
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
+}
+
+
+function clickHandler(renderNode, node){
+  if (renderNode.getAttribute("stroke") == null || renderNode.getAttribute("stroke") == "black"){
+    document.getElementById("buttons").innerHTML = "";
+    var elems = document.getElementsByTagName("circle");
+    for (var i=0; i<elems.length; i++){
+      elems[i].setAttribute("stroke", "black");
+      elems[i].setAttribute("stroke-width", 0);
+    }
+    renderNode.setAttribute("stroke", "red");
+    renderNode.setAttribute("stroke-width", 3);
+  } else{
+    document.getElementById("buttons").innerHTML = "";
+    renderNode.setAttribute("stroke", "black");
+    renderNode.setAttribute("stroke-width", 0);
+    return;
+  }
+
+  var nodeX = renderNode.getAttribute("cx");
+  var nodeY = renderNode.getAttribute("cy");
+  var lButton = document.createElement("button");
+  lButton.setAttribute("id", "leftChild");
+  lButton.innerHTML = "Add Left Child";
+  lButton.style.left = parseInt(nodeX) - 85;
+  lButton.style.top = parseInt(nodeY) + 65;
+  lButton.addEventListener("click", function(){addLeft(node)});
+  var rButton = document.createElement("button");
+  rButton.setAttribute("id", "rightChild");
+  rButton.innerHTML = "Add Right Child";
+  rButton.style.left = parseInt(nodeX) + 15;
+  rButton.style.top = parseInt(nodeY) + 65;
+  rButton.addEventListener("click", function(){addRight(node)});
+  var buttons = document.getElementById("buttons");
+  buttons.appendChild(lButton);
+  buttons.appendChild(rButton);
+}
+
+
+function addLeft(node){
+  globalTree.add(new Node(node), "left");
+  document.getElementById("svg").innerHTML = "";
+  document.getElementById("buttons").innerHTML = "";
+  var signature = preOrder(globalTree.root, "");
+  var display = document.getElementById("signatureDisplay");
+  display.innerHTML = "Current Binary Signature: "+signature+"\
+                      <br>Current Decimal Signature: "+ parseInt(signature, 2);
+  renderTree(globalTree.root, 30, null, null, 1);
+}
+
+
+function addRight(node){
+  globalTree.add(new Node(node), "right");
+  document.getElementById("svg").innerHTML = "";
+  document.getElementById("buttons").innerHTML = "";
+  var signature = preOrder(globalTree.root, "");
+  var display = document.getElementById("signatureDisplay");
+  display.innerHTML = "Current Binary Signature: "+signature+"\
+                      <br>Current Decimal Signature: "+ parseInt(signature, 2);
+  renderTree(globalTree.root, 30, null, null, 1);
+}
+
+var globalTree = new BinaryTree();
+var root = new Node(null);
+globalTree.add(root, null);
+var signature = preOrder(globalTree.root, "");
+var display = document.getElementById("signatureDisplay");
+display.innerHTML = "Current Binary Signature: "+signature+"\
+                    <br>Current Decimal Signature: "+ parseInt(signature, 2);
+renderTree(globalTree.root, 30, null, null, 1);
